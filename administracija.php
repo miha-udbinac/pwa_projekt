600e51afd9e6c945972782743a89d5b70ffdc355
miha-udbinac <miha.udbinac@gmail.com>
2026-06-30 14:10:18 +0200

    Update administracija.php

--- administracija.php
+++ administracija.php
@@ -2,20 +2,23 @@
 include 'connect.php';
 session_start();
 
-if (isset($_SESSION['username'])) {
-    echo '<p>Dobrodošli, <strong>' . $_SESSION['username'] . '</strong>! | <a href="logout.php">Odjavi se</a></p>';
-}
-
 if (!isset($_SESSION['level'])) {
-    echo '<h1>Pristup administraciji</h1>';
-    echo '<a href="login.php">Prijavi se</a> ili <a href="registracija.php">Registriraj se</a>';
-    echo '<br>';
+    echo '<body>';
+    echo '<main>';
+    echo '<header><h1>Pristup administraciji</h1></header>';
+    echo '<p>Za nastavak je potrebna prijava ili registracija.</p>';
+    echo '<a href="login.php">Prijavi se</a> | ';
+    echo '<a href="registracija.php">Registriraj se</a>';
+    echo '<br><br>';
     echo '<a href="index.php">Povratak na početnu</a>';
+    echo '</main>';
+    echo '</body>';
     exit();
 }
 
 if ($_SESSION['level'] != 1) {
     echo '<h2>Nemate administratorska prava!</h2>';
+    echo '<a href="logout.php">Odjavi se</a></p>';
     echo '<a href="index.php">Povratak na početnu</a>';
     exit();
 }
@@ -28,7 +31,11 @@ $result = mysqli_query($dbc, $query);
 <html lang="hr">
 <head><meta charset="UTF-8"><link rel="stylesheet" href="style.css"><title>Administracija</title></head>
 <body>
-    <header><h1>Administracija vijesti</h1></header>
+    <header><?php
+    if (isset($_SESSION['username'])) {
+    echo '<p>Dobrodošli, <strong>' . $_SESSION['username'] . '</strong>! | <a href="logout.php">Odjavi se</a></p>';
+    }
+    ?><h1>Administracija vijesti</h1></header>
     <main>
         <div><a href="unos.html">Unos nove vijesti</a></div>
         <?php
@@ -43,6 +50,16 @@ $result = mysqli_query($dbc, $query);
             </form><hr>';
         }
 
+        if(isset($_POST['delete'])) {
+            $id = $_POST['id'];
+            $stmt = mysqli_stmt_init($dbc);
+            $query = "DELETE FROM vijesti WHERE id=?";
+            if (mysqli_stmt_prepare($stmt, $query)) {
+                mysqli_stmt_bind_param($stmt, 'i', $id);
+                mysqli_stmt_execute($stmt);
+            }
+        }
+
         if(isset($_POST['update'])) {
             $id = $_POST['id'];
             $naslov = $_POST['title'];
@@ -53,16 +70,6 @@ $result = mysqli_query($dbc, $query);
             $query = "UPDATE vijesti SET naslov=?, sazetak=?, arhiva=? WHERE id=?";
             if (mysqli_stmt_prepare($stmt, $query)) {
                 mysqli_stmt_bind_param($stmt, 'ssii', $naslov, $sazetak, $arhiva, $id);
-                mysqli_stmt_execute($stmt);
-            }
-        }
-
-        if(isset($_POST['delete'])) {
-            $id = $_POST['id'];
-            $stmt = mysqli_stmt_init($dbc);
-            $query = "DELETE FROM vijesti WHERE id=?";
-            if (mysqli_stmt_prepare($stmt, $query)) {
-                mysqli_stmt_bind_param($stmt, 'i', $id);
                 mysqli_stmt_execute($stmt);
             }
         }
